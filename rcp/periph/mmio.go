d8db390c2a8561e8aa9eef656599d07814714f53
Timur Celik <mail@timurcelik.de>
2024-06-11 09:18:57 +0200

    Use struct embedding for periph registers

--- rcp/periph/mmio.go
+++ rcp/periph/mmio.go
@@ -8,45 +8,25 @@ import "embedded/mmio"
 // - 0x0500_0000 to 0x1fbf_ffff
 // - 0x1fd0_0000 to 0x7fff_ffff
 type U32 struct {
-	r mmio.U32
+	mmio.U32
 }
 
 func (r *U32) Store(v uint32) {
-	r.r.Store(v)
+	r.U32.Store(v)
 
-	for {
-		if regs.status.LoadBits(ioBusy) == 0 {
-			break
-		}
+	for regs.status.LoadBits(ioBusy) != 0 {
+		// wait
 	}
-}
-
-func (r *U32) Load() uint32 {
-	return r.r.Load()
-}
-
-func (r *U32) Addr() uintptr {
-	return r.r.Addr()
 }
 
 type R32[T mmio.T32] struct {
-	r mmio.R32[T]
+	mmio.R32[T]
 }
 
 func (r *R32[T]) Store(v T) {
-	r.r.Store(v)
+	r.R32.Store(v)
 
-	for {
-		if regs.status.LoadBits(ioBusy) == 0 {
-			break
-		}
+	for regs.status.LoadBits(ioBusy) != 0 {
+		// wait
 	}
-}
-
-func (r *R32[T]) Load() T {
-	return r.r.Load()
-}
-
-func (r *R32[T]) Addr() uintptr {
-	return r.r.Addr()
 }
