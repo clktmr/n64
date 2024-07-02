a635c561e61581e7836867e31d2a5fa92be5d5b1
Timur Celik <mail@timurcelik.de>
2024-07-02 11:52:54 +0200

    Fix mmio on PI bus with active DMA transfers
    Writing to mmio registers on the PI bus might fail silently if DMA is
    busy at the same time.

--- rcp/periph/mmio.go
+++ rcp/periph/mmio.go
@@ -7,26 +7,9 @@ import "embedded/mmio"
 // Use for MMIO on the PI external bus:
 // - 0x0500_0000 to 0x1fbf_ffff
 // - 0x1fd0_0000 to 0x7fff_ffff
+
 type U32 struct {
-	r mmio.U32
-}
-
-func (r *U32) Store(v uint32) {
-	r.r.Store(v)
-
-	for {
-		if regs.status.LoadBits(ioBusy) == 0 {
-			break
-		}
-	}
-}
-
-func (r *U32) Load() uint32 {
-	return r.r.Load()
-}
-
-func (r *U32) Addr() uintptr {
-	return r.r.Addr()
+	R32[uint32]
 }
 
 type R32[T mmio.T32] struct {
@@ -34,17 +17,23 @@ type R32[T mmio.T32] struct {
 }
 
 func (r *R32[T]) Store(v T) {
+	waitDMA()
 	r.r.Store(v)
-
-	for {
-		if regs.status.LoadBits(ioBusy) == 0 {
-			break
-		}
-	}
 }
 
 func (r *R32[T]) Load() T {
+	waitDMA()
 	return r.r.Load()
+}
+
+func (r *R32[T]) StoreBits(mask T, bits T) {
+	waitDMA()
+	r.r.StoreBits(mask, bits)
+}
+
+func (r *R32[T]) LoadBits(mask T) T {
+	waitDMA()
+	return r.r.LoadBits(mask)
 }
 
 func (r *R32[T]) Addr() uintptr {
