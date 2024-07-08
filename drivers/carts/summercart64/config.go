1c036a11daa65a77ebf57564da7894f4399f9209
Timur Celik <mail@timurcelik.de>
2024-07-08 13:15:59 +0200

    sc64: Fix reading of config values

--- drivers/carts/summercart64/config.go
+++ drivers/carts/summercart64/config.go
@@ -55,6 +55,6 @@ func (v *SummerCart64) SetConfig(option config, value uint32) (old uint32, err e
 }
 
 func (v *SummerCart64) Config(option config) (current uint32, err error) {
-	_, current, err = execCommand(cmdConfigSet, uint32(option), 0)
+	_, current, err = execCommand(cmdConfigGet, uint32(option), 0)
 	return
 }
